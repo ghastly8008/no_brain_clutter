1ef79650578dc4e0c2051eace19a035bbfa1563e
Alberto Namnum <ghastly8008@gmail.com>
2020-05-21 01:24:14 -0400

    both section working to create lists! Needs styling fix asap

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function addTask() {
     var count = 0;
     count++;
     var itemEle = document.createElement('li'); //create li element
-    itemEle.setAttribute("id", `item${count}`); //set id of item element   
+    itemEle.setAttribute("id", `task${count}`); //set id of item element   
     taskList.appendChild(itemEle);
 
     var itemValue = taskInput.value; //create variable = input value
@@ -41,7 +41,7 @@ function addTask() {
     taskInput.value = ''; //set input field blank after item submitted
 
     const itemBtn = document.createElement('button');
-    itemBtn.setAttribute("id", `item${count}`);
+    itemBtn.setAttribute("id", `taskBtn${count}`);
     itemBtn.innerHTML = "done";
     itemBtn.addEventListener('click', function (e) {
         taskList.removeChild(itemEle);
@@ -77,79 +77,72 @@ const noteDiv = document.getElementById('noteDiv');
 const noteInput = document.getElementById('noteInput');
 const noteDD = document.getElementById('noteDropDown');
 const noteBtn = document.getElementById('noteBtn');
-const noteListDiv = document.getElementById('notes');
+const typeList = document.getElementById('notes');
+const noteList = document.getElementById('noteList');
 
 noteBtn.addEventListener('click', noteBtnClk);
 
 //create type list for noteBtn
 var noteType = ['Note', 'Idea', 'Reflection', 'Self Feedback', 'Other'];
 for (var i = 0; i < 5; i++) {
-    var note = document.createElement('p');
-    note.setAttribute("id", `type${i}`);
-    note.setAttribute("class", 'type');
-    note.innerHTML = noteType[i];
-    //note.addEventListener('click', addNote);
-    //note.addEventListener('click', hideNoteList);
-    noteListDiv.appendChild(note);
+    var type = document.createElement('p');
+    type.setAttribute("id", `type${i}`);
+    type.setAttribute("class", 'type');
+    type.innerHTML = noteType[i];
+    type.addEventListener('click', addNote);
+    type.addEventListener('click', hideNoteList);
+    typeList.appendChild(type);
 };
+
+//create element and check off button and add to task list
+function addNote() {
+    if (noteInput.value == '') {
+        return null
+    }
+    //daysList.classList.toggle("show");   
+    var count2 = 0;
+    count2++;
+    var note = document.createElement('li'); //create li element
+    note.setAttribute("id", `note${count2}`); //set id of item element   
+
+    var noteValue = noteInput.value; //create variable = input value
+    note.innerHTML = noteValue; //set li html = input value
+
+    noteList.appendChild(note);
+
+    noteInput.value = ''; //set input field blank after item submitted
+
+    const noteBtn = document.createElement('button');
+    noteBtn.setAttribute("id", `noteBtn${count2}`);
+    noteBtn.innerHTML = "done";
+    noteBtn.addEventListener('click', function (e) {
+    noteList.removeChild(note);
+    });
+
+    note.appendChild(noteBtn);
+
+}
+
+//add click and enter key events to submit button
+window.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
+        addNote();
+    }
+});
+
+//make days list disapear if task button is hit again or day is selected
 
 //function to show and hide note list when noteBtn is clicked
 function noteBtnClk() {
     if (noteInput.value == '') {
         return null
     }
-    noteListDiv.classList.toggle("show2");
+    typeList.classList.toggle("show2");
 }
 
-
-/////Add notes ////
-
-//create element and check off button and add to task list
-function addTask() {
-    if (taskInput.value == '') {
-        return null
-    }
-    //daysList.classList.toggle("show");   
-    var count = 0;
-    count++;
-    var itemEle = document.createElement('li'); //create li element
-    itemEle.setAttribute("id", `item${count}`); //set id of item element   
-    taskList.appendChild(itemEle);
-
-    var itemValue = taskInput.value; //create variable = input value
-    itemEle.innerHTML = itemValue; //set li html = input value
-
-    taskInput.value = ''; //set input field blank after item submitted
-
-    const itemBtn = document.createElement('button');
-    itemBtn.setAttribute("id", `item${count}`);
-    itemBtn.innerHTML = "done";
-    itemBtn.addEventListener('click', function (e) {
-        taskList.removeChild(itemEle);
-    });
-
-    itemEle.appendChild(itemBtn);
+function hideNoteList() {
+    typeList.classList.toggle("show2");
 }
-
-//add click and enter key events to submit button
-window.addEventListener('keypress', function (e) {
-    if (e.keyCode === 13) {
-        addTask();
-    }
-});
-
-//make days list disapear if task button is hit again or day is selected
-
-function hideDaysList() {
-    daysList.classList.toggle("show");
-}
-
-function ddBtnHov() {
-    if (taskInput.value == '') {
-        return null
-    }
-    daysList.classList.toggle("show");
-}   
 
 
 
