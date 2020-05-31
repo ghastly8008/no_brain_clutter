647d03a4e5e3239175ee1f7e8ee14c8f565b6f86
Alberto Namnum <ghastly8008@gmail.com>
2020-05-31 02:09:14 -0400

    All edit label stuff now in seperate js file

--- edit_note_labels.js
+++ edit_note_labels.js
@@ -1,4 +1,19 @@
 
+///***//edit note label section//***///
+
+const EDIT_LABEL_SECTION = document.getElementById('note__editLabel__div');
+
+const EDIT_LABEL_BTN = document.createElement('p');
+EDIT_LABEL_BTN.innerHTML = " Edit Labels";
+EDIT_LABEL_BTN.setAttribute("id", 'editLabelLink--note');
+EDIT_LABEL_BTN.setAttribute("class", 'body__DD__element');
+NOTE_DD_LIST.appendChild(EDIT_LABEL_BTN);
+EDIT_LABEL_BTN.addEventListener('click', displayLabelEditSection);
+
+
+function displayLabelEditSection() {
+    EDIT_LABEL_SECTION.classList.toggle("show");
+}
 
 //add label button feature
 const ADD_LABEL_BTN = document.getElementById("button__editLabel__add--note");
@@ -8,3 +23,71 @@ ADD_LABEL_BTN.addEventListener('click', function (e) {
     NEW_LABEL_INPUT.setAttribute("id", `noteType${LABEL_LIST.childNodes.length}`)
     LABEL_LIST.appendChild(NEW_LABEL_INPUT);
 });
+
+//remove label button feature
+
+const REMOVE_LABEL_BTN = document.getElementById("button__editLabel__remove--note");
+
+REMOVE_LABEL_BTN.addEventListener('click', function (e) {
+    let menu = LABEL_LIST;
+    menu.removeChild(menu.lastElementChild);
+});
+
+//update button feature which updates noteType elements
+
+const UPDATE_LABEL_BTN = document.getElementById("button_editLabel__update--note");
+UPDATE_LABEL_BTN.addEventListener('click', updateNoteType);
+UPDATE_LABEL_BTN.addEventListener('click', updateNoteDD);
+
+function updateNoteType() {
+    var item = LABEL_LIST.getElementsByTagName("li");
+    for (var i = 0; i < item.length; i++) {
+        noteType.push(item[i]);
+    }
+    EDIT_LABEL_SECTION.classList.toggle("show");
+}
+
+const DEFAULT_LABEL_BTN = document.getElementById('button_editLabel__default--note');
+DEFAULT_LABEL_BTN.addEventListener('click', function (e) {
+    EDIT_LABEL_SECTION.classList.toggle("show");
+});
+
+//return list to original state when you hit default button
+
+DEFAULT_LABEL_BTN.addEventListener('click', returnToInitialState);
+
+function returnToInitialState() {
+    while (LABEL_LIST.firstChild) {
+        LABEL_LIST.removeChild(LABEL_LIST.lastChild);
+    }
+    for (var i = 0; i < (noteType.length); i++) {
+        const LABEL_INPUT = document.createElement('input');
+        LABEL_INPUT.setAttribute("id", `noteType${i}`)
+        LABEL_INPUT.value = noteType[i];
+        LABEL_LIST.appendChild(LABEL_INPUT);
+    }
+}
+
+var updNoteType = [];
+NOTE_BTN.addEventListener('click', updateNoteDD);
+
+function updateNoteDD() {
+    updNoteType = [];
+    while (NOTE_DD_LIST.lastChild.id !== 'editLabelLink--note') {
+        NOTE_DD_LIST.removeChild(NOTE_DD_LIST.lastChild);
+    } //<-- remove all child nodes of DD list except the "edit labels" one
+    
+    for (var i = 0; i < LABEL_LIST.childNodes.length; i++) {
+        var test = document.getElementById(`noteType${i}`);
+        updNoteType.push(test.value);
+    }
+    for (var i = 0; i < LABEL_LIST.childNodes.length; i++) {
+        var type = document.createElement('p');
+        type.setAttribute("id", `body__DD__element--note${i}`);
+        type.setAttribute("class", 'body__DD__element');
+        type.innerHTML = updNoteType[i];
+        type.addEventListener('click', addItemToList);
+        type.addEventListener('click', hideDDList);
+        NOTE_DD_LIST.appendChild(type);
+    }
+}
--- main.js
+++ main.js
@@ -93,7 +93,6 @@ function removeElement(elem) {
 const TODO_BTN = document.getElementById('body__btn--todo');
 const TODO_DD_LIST = document.getElementById('body__DD--todo');
 
-
 TODO_BTN.addEventListener('click', showDDList);
 TODO_BTN.addEventListener('click', getBtnClicked);
 
@@ -115,25 +114,7 @@ const NOTE_BTN = document.getElementById('body__btn--note');
 const NOTE_DD_LIST = document.getElementById('body__DD--note');
 
 NOTE_BTN.addEventListener('click', getBtnClicked);
-NOTE_BTN.addEventListener('click', updateNoteDD);
 NOTE_BTN.addEventListener('click', showDDList);
-
-///***//edit note label section//***///
-
-const EDIT_LABEL_SECTION = document.getElementById('note__editLabel__div');
-
-const EDIT_LABEL_BTN = document.createElement('p');
-EDIT_LABEL_BTN.innerHTML = " Edit Labels";
-EDIT_LABEL_BTN.setAttribute("id", 'editLabelLink--note');
-EDIT_LABEL_BTN.setAttribute("class", 'body__DD__element');
-NOTE_DD_LIST.appendChild(EDIT_LABEL_BTN);
-EDIT_LABEL_BTN.addEventListener('click', displayLabelEditSection);
-
-//function that displays editLabelsSection
-
-function displayLabelEditSection() {
-    EDIT_LABEL_SECTION.classList.toggle("show");
-}
 
 //create edit labels section with orginal "noteType" list
 var noteType = ['Note', 'Idea', 'Reflection', 'Self Feedback', 'Other'];
@@ -146,96 +127,6 @@ for (var i = 0; i < (noteType.length); i++) {
     LABEL_INPUT.value = noteType[i];
     LABEL_LIST.appendChild(LABEL_INPUT);
 }
-
-//add label button feature
-// const ADD_LABEL_BTN = document.getElementById("button__editLabel__add--note");
-
-// ADD_LABEL_BTN.addEventListener('click', function (e) {
-//     const NEW_LABEL_INPUT = document.createElement('input');
-//     NEW_LABEL_INPUT.setAttribute("id", `noteType${LABEL_LIST.childNodes.length}`)
-//     LABEL_LIST.appendChild(NEW_LABEL_INPUT);
-// });
-
-//return list to previous state and cancel when you hit cancel
-
-const CANCEL_LABEL_BTN = document.getElementById('button_editLabel__default--note');
-CANCEL_LABEL_BTN.addEventListener('click', function (e) {
-    EDIT_LABEL_SECTION.classList.toggle("show");
-});
-
-CANCEL_LABEL_BTN.addEventListener('click', returnToInitialState);
-
-function returnToInitialState() {
-    while (LABEL_LIST.firstChild) {
-        LABEL_LIST.removeChild(LABEL_LIST.lastChild);
-    }
-    for (var i = 0; i < (noteType.length); i++) {
-        const LABEL_INPUT = document.createElement('input');
-        LABEL_INPUT.setAttribute("id", `noteType${i}`)
-        LABEL_INPUT.value = noteType[i];
-        LABEL_LIST.appendChild(LABEL_INPUT);
-    }
-}
-
-//remove label button feature
-
-const REMOVE_LABEL_BTN = document.getElementById("button__editLabel__remove--note");
-
-REMOVE_LABEL_BTN.addEventListener('click', function (e) {
-    let menu = LABEL_LIST;
-    menu.removeChild(menu.lastElementChild);
-});
-
-//update button feature which updates noteType elements
-
-const UPDATE_LABEL_BTN = document.getElementById("button_editLabel__update--note");
-UPDATE_LABEL_BTN.addEventListener('click', updateNoteType);
-UPDATE_LABEL_BTN.addEventListener('click', updateNoteDD);
-
-function updateNoteType() {
-    var item = LABEL_LIST.getElementsByTagName("li");
-    for (var i = 0; i < item.length; i++) {
-        noteType.push(item[i]);
-    }
-    EDIT_LABEL_SECTION.classList.toggle("show");
-}
-
-//create new array out of items in input fields in order to repopuate list with
-
-var updNoteType = [];
-
-function updateNoteDD() {
-    updNoteType = [];
-    while (NOTE_DD_LIST.lastChild.id !== 'editLabelLink--note') {
-        NOTE_DD_LIST.removeChild(NOTE_DD_LIST.lastChild);
-    }
-    for (var i = 0; i < LABEL_LIST.childNodes.length; i++) {
-        var test = document.getElementById(`noteType${i}`);
-        updNoteType.push(test.value);
-    }
-    for (var i = 0; i < LABEL_LIST.childNodes.length; i++) {
-        var type = document.createElement('p');
-        type.setAttribute("id", `body__DD__element--note${i}`);
-        type.setAttribute("class", 'body__DD__element');
-        type.innerHTML = updNoteType[i];
-        type.addEventListener('click', addItemToList);
-        type.addEventListener('click', hideDDList);
-        NOTE_DD_LIST.appendChild(type);
-    }
-}
-
-// function showNoteDDList (){
-// for (var i = 0; i < LABEL_LIST.childNodes.length; i++) {
-//     var type = document.createElement('p');
-//     type.setAttribute("id", `body__DD__element--note${i}`);
-//     type.setAttribute("class", 'body__DD__element');
-//     type.innerHTML = updNoteType[i];
-//     type.addEventListener('click', addItemToList);
-//     // type.addEventListener('click', hideDDList);
-//     NOTE_DD_LIST.appendChild(type);
-//     console.log(type);
-// }
-// }
 
 ///***///PERSONAL GOALS SECTION////***/////
 
